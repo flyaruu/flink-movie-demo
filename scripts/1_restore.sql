8430c1c9968ffee3b34806cf7ddcd6f35b05ac2a
Frank Lyaruu <f.lyaruu@quin.nl>
2022-03-20 09:54:40 +0100

    moved store to scripts

--- scripts/1_restore.sql
+++ scripts/1_restore.sql
@@ -323,37 +323,6 @@ CREATE TABLE language (
 ALTER TABLE public.language OWNER TO postgres;
 
 
-
-
---
--- Name: store_store_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE store_store_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.store_store_id_seq OWNER TO postgres;
-
---
--- Name: store; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE store (
-    store_id integer DEFAULT nextval('store_store_id_seq'::regclass) NOT NULL,
-    manager_staff_id smallint NOT NULL,
-    address_id smallint NOT NULL,
-    last_update timestamp without time zone DEFAULT now() NOT NULL
-);
-
-
-ALTER TABLE public.store OWNER TO postgres;
-
-
 --
 -- Data for Name: actor; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -480,21 +449,6 @@ SELECT pg_catalog.setval('language_language_id_seq', 6, true);
 
 
 --
--- Data for Name: store; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY store (store_id, manager_staff_id, address_id, last_update) FROM stdin;
-\.
-COPY store (store_id, manager_staff_id, address_id, last_update) FROM '/docker-entrypoint-initdb.d/store.dat';
-
---
--- Name: store_store_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('store_store_id_seq', 2, true);
-
-
---
 -- Name: actor_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -569,12 +523,6 @@ ALTER TABLE ONLY language
 
 
 
---
--- Name: store_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY store
-    ADD CONSTRAINT store_pkey PRIMARY KEY (store_id);
 
 
 --
@@ -630,13 +578,6 @@ CREATE INDEX idx_fk_language_id ON film USING btree (language_id);
 --
 
 CREATE INDEX idx_title ON film USING btree (title);
-
-
---
--- Name: idx_unq_manager_staff_id; Type: INDEX; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE UNIQUE INDEX idx_unq_manager_staff_id ON store USING btree (manager_staff_id);
 
 
 --
@@ -712,13 +653,6 @@ CREATE TRIGGER last_updated BEFORE UPDATE ON language FOR EACH ROW EXECUTE PROCE
 
 
 --
--- Name: last_updated; Type: TRIGGER; Schema: public; Owner: postgres
---
-
-CREATE TRIGGER last_updated BEFORE UPDATE ON store FOR EACH ROW EXECUTE PROCEDURE last_updated();
-
-
---
 -- Name: film_actor_actor_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -774,13 +708,6 @@ ALTER TABLE ONLY city
     ADD CONSTRAINT fk_city FOREIGN KEY (country_id) REFERENCES country(country_id);
 
 
-
---
--- Name: store_address_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY store
-    ADD CONSTRAINT store_address_id_fkey FOREIGN KEY (address_id) REFERENCES address(address_id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 
 
---
+++ scripts/6_store.sql
@@ -0,0 +1,75 @@
+
+
+
+--
+-- Name: store_store_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE store_store_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.store_store_id_seq OWNER TO postgres;
+
+--
+-- Name: store; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE store (
+    store_id integer DEFAULT nextval('store_store_id_seq'::regclass) NOT NULL,
+    manager_staff_id smallint NOT NULL,
+    address_id smallint NOT NULL,
+    last_update timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.store OWNER TO postgres;
+
+
+--
+-- Data for Name: store; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY store (store_id, manager_staff_id, address_id, last_update) FROM stdin;
+\.
+COPY store (store_id, manager_staff_id, address_id, last_update) FROM '/docker-entrypoint-initdb.d/store.dat';
+
+--
+-- Name: store_store_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('store_store_id_seq', 2, true);
+
+--
+-- Name: store_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY store
+    ADD CONSTRAINT store_pkey PRIMARY KEY (store_id);
+
+
+--
+-- Name: idx_unq_manager_staff_id; Type: INDEX; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE UNIQUE INDEX idx_unq_manager_staff_id ON store USING btree (manager_staff_id);
+
+
+--
+-- Name: last_updated; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER last_updated BEFORE UPDATE ON store FOR EACH ROW EXECUTE PROCEDURE last_updated();
+
+--
+-- Name: store_address_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY store
+    ADD CONSTRAINT store_address_id_fkey FOREIGN KEY (address_id) REFERENCES address(address_id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
