7dedd1a7fdab30d2d644363b7c65a1f6dee3f8d4
Frank Lyaruu <f.lyaruu@quin.nl>
2022-03-20 09:21:36 +0100

    move staff to script

--- scripts/1_restore.sql
+++ scripts/1_restore.sql
@@ -386,42 +386,6 @@ ALTER TABLE public.language OWNER TO postgres;
 
 
 --
--- Name: staff_staff_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE staff_staff_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.staff_staff_id_seq OWNER TO postgres;
-
---
--- Name: staff; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE staff (
-    staff_id integer DEFAULT nextval('staff_staff_id_seq'::regclass) NOT NULL,
-    first_name character varying(45) NOT NULL,
-    last_name character varying(45) NOT NULL,
-    address_id smallint NOT NULL,
-    email character varying(50),
-    salary smallint,
-    store_id smallint NOT NULL,
-    active boolean DEFAULT true NOT NULL,
-    username character varying(16) NOT NULL,
-    password character varying(40),
-    last_update timestamp without time zone DEFAULT now() NOT NULL,
-    picture bytea
-);
-
-
-ALTER TABLE public.staff OWNER TO postgres;
-
---
 -- Name: store_store_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -603,22 +567,6 @@ SELECT pg_catalog.setval('language_language_id_seq', 6, true);
 
 
 
-
---
--- Data for Name: staff; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY staff (staff_id, first_name, last_name, address_id, email, store_id, active, username, password, last_update, picture) FROM stdin;
-\.
-COPY staff (staff_id, first_name, last_name, address_id, email, store_id, active, username, password, last_update, picture) FROM '/docker-entrypoint-initdb.d/staff.dat';
-
---
--- Name: staff_staff_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('staff_staff_id_seq', 2, true);
-
-
 --
 -- Data for Name: store; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -721,13 +669,6 @@ ALTER TABLE ONLY inventory
 ALTER TABLE ONLY language
     ADD CONSTRAINT language_pkey PRIMARY KEY (language_id);
 
-
---
--- Name: staff_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY staff
-    ADD CONSTRAINT staff_pkey PRIMARY KEY (staff_id);
 
 
 --
@@ -888,13 +829,6 @@ CREATE TRIGGER last_updated BEFORE UPDATE ON language FOR EACH ROW EXECUTE PROCE
 -- Name: last_updated; Type: TRIGGER; Schema: public; Owner: postgres
 --
 
-CREATE TRIGGER last_updated BEFORE UPDATE ON staff FOR EACH ROW EXECUTE PROCEDURE last_updated();
-
-
---
--- Name: last_updated; Type: TRIGGER; Schema: public; Owner: postgres
---
-
 CREATE TRIGGER last_updated BEFORE UPDATE ON store FOR EACH ROW EXECUTE PROCEDURE last_updated();
 
 
@@ -964,27 +898,12 @@ ALTER TABLE ONLY inventory
 
 
 --
--- Name: staff_address_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY staff
-    ADD CONSTRAINT staff_address_id_fkey FOREIGN KEY (address_id) REFERENCES address(address_id) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-
---
 -- Name: store_address_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY store
     ADD CONSTRAINT store_address_id_fkey FOREIGN KEY (address_id) REFERENCES address(address_id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
-
---
--- Name: store_manager_staff_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY store
-    ADD CONSTRAINT store_manager_staff_id_fkey FOREIGN KEY (manager_staff_id) REFERENCES staff(staff_id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 
 --
