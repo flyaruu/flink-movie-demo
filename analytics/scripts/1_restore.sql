d02a54fa471364f4b80107d39ece7b01cf52d47e
Frank Lyaruu <f.lyaruu@quin.nl>
2022-04-01 16:59:47 +0200

    add analytics database. Use images

---
+++ analytics/scripts/1_restore.sql
@@ -0,0 +1,89 @@
+--
+-- NOTE:
+--
+-- File paths need to be edited. Search for /docker-entrypoint-initdb.d and
+-- replace it with the path to the directory containing
+-- the extracted data files.
+--
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+
+ALTER SCHEMA public OWNER TO postgres;
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON SCHEMA public IS 'Standard public schema';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+
+
+--
+-- Name: last_updated(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION last_updated() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    NEW.last_update = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END $$;
+
+
+ALTER FUNCTION public.last_updated() OWNER TO postgres;--
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+
+
+
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ analytics/scripts/2_analytics.sql
@@ -0,0 +1,7 @@
+
+
+CREATE TABLE topscores (
+    first_name character varying(45) NOT NULL,
+    last_name character varying(45) NOT NULL,
+    score FLOAT
+);
