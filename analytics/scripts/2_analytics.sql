1adec53956678513f5875e12713b16e89efe4da8
Frank Lyaruu <f.lyaruu@quin.nl>
2022-07-19 18:09:55 +0200

    use local build for analytics + add table to analytics

--- analytics/scripts/2_analytics.sql
+++ analytics/scripts/2_analytics.sql
@@ -5,3 +5,15 @@ CREATE TABLE topscores (
     last_name character varying(45) NOT NULL,
     score FLOAT
 );
+
+CREATE TABLE rental_anonymized (
+	film_id integer,
+	agebracket integer,
+	rental_count integer
+);
+
+create table rental_by_age (
+	agebracket BIGINT primary key,
+	rental_count BIGINT
+);
+
