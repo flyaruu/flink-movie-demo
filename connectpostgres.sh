485e99c4cdc7225bb9a1b0adc6ab132091997609
Frank Lyaruu <f.lyaruu@quin.nl>
2022-08-31 15:27:49 +0200

    clean to reproducing issue

--- connectpostgres.sh
+++ connectpostgres.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
 curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d @debezium_avro/film.json localhost:8083/connectors/
-curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d @debezium_avro/customer.json localhost:8083/connectors/
-curl -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d @debezium_avro/rental.json localhost:8083/connectors/
